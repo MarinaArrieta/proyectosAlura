ecfbe28a52c81e99b2928f556fcedaa0296c3aba
MarinaArrieta <arrietamarina12@gmail.com>
2024-10-27 21:44:20 -0300

    Eliminacion de archivos innecesarios, eliminacion de comentarios, agregado de archivo enviar personajes

--- umaru/js/conexionAPI.js
+++ umaru/js/conexionAPI.js
@@ -1,12 +1,26 @@
 async function umaruChan() {
     const conexion = await fetch("http://localhost:3001/umaru");
     const conexionConvertida = conexion.json();
-    // console.log(conexionConvertida);
 
     return conexionConvertida;
 }
 
-// umaruChan();
+async function crearCharacters(nombre,nombreJapones,tipo,descripcion,imagen){
+    const conexion = await fetch("http://localhost:3001/umaru",{
+        method:"POST",
+        headers:{"Content-type":"aplication/json"},
+        body:JSON.stringify({
+            nombre:nombre,
+            nombreJapones:nombreJapones,
+            tipo:tipo,
+            descripcion:descripcion,
+            imagen:imagen
+        })
+    })
+    const conexionConvertida=conexion.json();
+    return conexionConvertida;
+}
+
 export const conexionAPI={
-    umaruChan
+    umaruChan,crearCharacters
 }
